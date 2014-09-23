5091a9ffd9146b377e85de3f3f9f27d5fb98435f
Steen Hulthin Rasmussen <steenhulthin@gmail.com>
2014-09-23 23:10:54 +0200

    Now includes selfmeasuring.

--- HelloBreadboardX1Module/Program.cs
+++ HelloBreadboardX1Module/Program.cs
@@ -19,13 +19,18 @@ namespace HelloBreadboardX1Module
     public partial class Program
     {
         private DigitalInput _digitalInput; 
-        private DigitalOutput _digitalOutput; 
+        private DigitalOutput _digitalOutput;
+        private DigitalInput _digitalInput2; 
+        private DigitalOutput _digitalOutput2;
+        private bool _analogValue = false;
         void ProgramStarted()
         {
             Debug.Print("Program Started");
 
             _digitalInput = breadBoard_X1.SetupDigitalInput(GT.Socket.Pin.Three, GlitchFilterMode.On, ResistorMode.Disabled);
             _digitalOutput = breadBoard_X1.SetupDigitalOutput(GT.Socket.Pin.Four, false);
+            _digitalInput2 = breadBoard_X1.SetupDigitalInput(GT.Socket.Pin.Six, GlitchFilterMode.On, ResistorMode.Disabled);
+            _digitalOutput2 = breadBoard_X1.SetupDigitalOutput(GT.Socket.Pin.Seven, false);
 
             var timer = new GT.Timer(50);
             timer.Tick += timer_Tick;
@@ -37,6 +42,10 @@ namespace HelloBreadboardX1Module
             var isButtonPressed = _digitalInput.Read();
             Mainboard.SetDebugLED(isButtonPressed);
             _digitalOutput.Write(isButtonPressed);
+            
+            _digitalOutput2.Write(_analogValue);
+            Debug.Print("signal measured: " + _digitalInput2.Read().ToString());
+            _analogValue = !_analogValue;
         }
     }
 }
